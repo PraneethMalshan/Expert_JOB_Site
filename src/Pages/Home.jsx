10b410339f417a7bf305f496057332c5028cbefd
PraneethMalshan <praneethmalshan12345@gmail.com>
2024-01-28 02:12:44 +0530

    AddedJobCards

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,8 @@
 import {useEffect, useState} from 'react'
 import Banner from '../components/Banner'
+import { data } from 'autoprefixer';
+import Card from '../components/Card';
+import Jobs from './Jobs';
 
 const Home = () => {
     const [selectedCategory, setSelectedCategory] = useState(null);
@@ -21,11 +24,60 @@ const Home = () => {
 
 // Filter jobs title
     const filteredItems = jobs.filter((jobs) => jobs.jobTitle.toLowerCase().indexOf(query.toLocaleLowerCase()) !== -1);
-    // console.log(filteredItems)
+
+
+    // ===Radio Filtering===
+    const handleChange = (event) => {
+        setSelectedCategory(event.target.value)
+    }
+
+    // ===Button based Filtering===
+    const andleClick = (event) => {
+        setSelectedCategory(event.target.value)
+    }
+
+    // Main function 
+    const filteredData = (jobs, selected, query) => {
+        let filteredJobs = jobs;
+        // filtering input items
+        if(query){
+            filteredJobs =filteredItems;
+        }
+
+        // category filtering
+        if(selected){
+            filteredJobs = filteredJobs.filter(({jobLocation, maxPrice, experienceLevel, salaryType, employmentType, postingDate}) => {
+                jobLocation.toLowerCase() <= selected.toLowerCase() ||
+                parseInt(maxPrice) === parseInt(selected) ||
+                employmentType.toLowerCase() === selected.toLowerCase()
+            });
+            console.log(filteredJobs);
+        }
+
+        return filteredJobs.map((data, i) => <Card key={i} data={data}/>)
+
+
+    }
+
+    const result = filteredData(jobs, selectedCategory, query);
+
 
   return (
     <div>
         <Banner query={query} handleInputChange={handleInputChange}/>
+
+{/* Main Content */}
+        <div className='bg-[#000000] md:grid grid-cols-4 gap-8 lg:px-24 px-4 py-12 ' >
+            {/* Left Side */}
+            <div className='p-4 bg-[#121212] rounded'>Left</div>
+
+            {/* Job Cards */}
+            <div className='col-span-2 p-4 bg-[#121212] rounded-sm'><Jobs result={result}/></div>
+            
+            
+            {/* Right Side */}
+            <div className='p-4 bg-[#121212] rounded'>Right</div>
+        </div>
     </div>
   )
 }
---
+++ src/components/Card.jsx
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom';
+import { FiCalendar, FiClock, FiDollarSign, FiMapPin } from 'react-icons/fi';
+
+const Card = ({data}) => {
+    const {companyName, jobTitle, companyLogo, minPrice, maxPrice, salaryType, jobLocation, employmentType, postingDate, description, } = data;
+  return (
+    // <div>
+    //   <h3>{data.jobTitle}</h3>
+    // </div>
+
+    <section className='card'>
+        <Link to={"/"} className='flex flex-col items-start gap-4 sm:flex-row'>
+            <img src={companyLogo} alt="" />
+            <div>
+                <h4 className='mb-1 text-white'>{companyName}</h4>
+                <h3 className='mb-2 text-lg font-semibold text-white'>{jobTitle}</h3>
+
+                <div className='flex flex-wrap gap-2 mb-2 text-base text-white/70'>
+                    <span className='flex items-center gap-2 '><FiMapPin/>{jobLocation}</span>
+                    <span className='flex items-center gap-2 '><FiClock/>{employmentType}</span>
+                    <span className='flex items-center gap-2 '><FiDollarSign/>{minPrice}-{maxPrice}</span>
+                    <span className='flex items-center gap-2 '><FiCalendar/>{postingDate}</span>
+                </div>
+
+                <p className='text-base text-white/70 '>{description}</p>
+                
+            </div>
+        </Link>
+    </section>
+
+  )
+}
+
+export default Card
