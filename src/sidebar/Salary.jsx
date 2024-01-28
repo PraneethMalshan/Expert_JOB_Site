c61fccc614f7fc8cd6b67cbfa6c0e261212721e9
PraneethMalshan <praneethmalshan12345@gmail.com>
2024-01-28 13:53:31 +0530

    Added Salary Slide

--- src/sidebar/Salary.jsx
+++ src/sidebar/Salary.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from './Button'
+import InputField from '../components/InputField'
 
 function Salary({handleChange, handleClick}) {
   return (
@@ -10,8 +11,49 @@ function Salary({handleChange, handleClick}) {
         <Button onClickHandler={handleClick} value="" title="Hourly"/>
         <Button onClickHandler={handleClick} value="Monthly" title="Monthly"/>
         <Button onClickHandler={handleClick} value="Yearly" title="Yearly"/>
-
       </div>
+      
+      <div>
+      <label className="text-white sidebar-label-container">
+                <input 
+                  type="radio" 
+                  name="test" 
+                  id="test" 
+                  value="" 
+                  onChange={handleChange} 
+                  />
+                <span className="checkmark"></span>All
+            </label>
+
+            <InputField 
+              handleChange={handleChange} 
+              value={30} 
+              title="< 30000k" 
+              name="test2"
+              />
+              
+            <InputField 
+              handleChange={handleChange} 
+              value={50} 
+              title="< 50000k" 
+              name="test2"
+              />
+
+            <InputField 
+              handleChange={handleChange} 
+              value={80} 
+              title="< 80000k" 
+              name="test2"
+              />
+
+            <InputField 
+              handleChange={handleChange} 
+              value={100} 
+              title="< 1000000k" 
+              name="test2"
+              />
+      </div>
+
     </div>
   )
 }
