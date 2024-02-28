1f5765ddb11bf565c6bd67f1cc3a620e3f23de67
PraneethMalshan <praneethmalshan12345@gmail.com>
2024-02-28 13:38:12 +0530

    Added Salary search Part

--- Expert_Job_Client/src/Pages/SalaryPage.jsx
+++ Expert_Job_Client/src/Pages/SalaryPage.jsx
@@ -1,15 +1,33 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import PageHeader from '../components/PageHeader'
 
 const SalaryPage = () => {
+    const [searchText, setSearchText] = useState("");
+    const [salary, setSalary] = useState([]);
+    
+    useEffect( () => {
+        fetch("salary.json").then(res => res.json()).then(data => setSalary(data))
+    }, [])
+
+    const handleSearch = () => {
+        const filter = salary.filter(
+            (job) => 
+                job.title.toLowerCase().indexOf(searchText.toLowerCase()) !== -1
+                );
+                console.log(filter)
+                setSalary(filter)
+    }
+
+    console.log(searchText)
+    
   return (
     <div className='container px-4 mx-auto text-white max-w-screen-2xl xl:px-24'>
         <PageHeader title={"Estimate Salary"} path={"Salary"}/>
 
         <div className='mt-5'>
             <div className='p-2 mb-2 text-center search-box'>
-                <input type='text' name='search' id='search' className='w-full py-2 pl-3 mb-4 bg-black border focus:outline-none lg:w-6/12'/>
-                <button className='px-8 py-2 mb-4 font-semibold text-white bg-blue-500 rounded-sm'>Search</button>
+                <input type='text' name='search' id='search' className='w-full py-2 pl-3 mb-4 bg-black border focus:outline-none lg:w-6/12' onChange={e => setSearchText(e.target.value)}/>
+                <button onClick={handleSearch} className='px-8 py-2 mb-4 font-semibold text-white bg-blue-500 rounded-sm'>Search</button>
             </div>
         </div>
     </div>
